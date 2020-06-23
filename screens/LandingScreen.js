f1db81daaf096d7c89148503f3a426baa4de9825
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-06-23 18:00:34 +0100

    added placeholder text

---
+++ screens/LandingScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>List of workouts</Text>
+      {/* <TouchableOpacity style={styles.relativeCard} onPress={() => navigation.navigate('WorkoutScreen')}></TouchableOpacity> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

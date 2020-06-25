f55b7cc4774a5238c75513ffdf61a0881ffcae18
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-06-25 17:29:28 +0100

    added padding

--- screens/WorkoutScreen.js
+++ screens/WorkoutScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { ProgressChart } from 'react-native-chart-kit';
+import ProgressCircle from 'react-native-progress-circle'
+
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { useFonts } from '@use-expo/font';
@@ -20,86 +22,133 @@ export default function Workout({ navigation }) {
   } else {
     return (
       <View style={styles.container}>
-        <View style={styles.top}>
-          <Text style={styles.topText}>Track your progress</Text>
-          <View style={styles.whiteBlock}></View>
-        </View>
-        <View style={styles.bottom}>
-          <View style={styles.bottomTextContainer}>
-            <Text style={styles.bottomText}>Exercise Breakdown</Text>
+        <ScrollView>
+          <View style={styles.top}>
+            <View style={styles.textContainer}>
+              <Text style={styles.topText}>Track your progress</Text>
+            </View>
+            <View style={styles.whiteBlock}>
+              <View style={styles.chartContainer}>
+                {/* <ProgressChart data={data} width={200} height={200} strokeWidth={16} radius={32}
+              />  */}
+                <ProgressCircle
+                  percent={25}
+                  radius={50}
+                  borderWidth={10}
+                  color="#6699ff"
+                  bgColor="#fff"
+                  shadowColor='#dadada'
+                  sty
+                >
+                  <Text style={{ fontSize: 15 }}>{'25%'}</Text>
+                </ProgressCircle>
+              </View>
+              <View style={styles.infoContainer}>
+
+                <View style={styles.caloriesContainer} >
+                  <Text style={styles.infoText}>
+                    Calories
+               </Text>
+                  <Text style={styles.infoText}>460KJ
+               </Text>
+                </View>
+                <View style={styles.timeContainer}>
+                  <Text style={styles.infoText}>
+                    Total Time
+               </Text>
+                  <Text style={styles.infoText}>
+                    40 mins
+               </Text>
+                </View>
+              </View>
+
+              <View style={styles.buttonContainer}>
+
+              </View>
+            </View>
           </View>
-          <ScrollView style={styles.exerciseList}>
-            <TouchableOpacity
-              style={styles.rect}
-              activeOpacity={0.6}
-              onPress={() => navigation.navigate("Bench Press")} >
-              <View style={styles.inner} >
-                <View style={styles.text} >
-                  <Text style={styles.mainText}>Bench Press</Text>
+          <View style={styles.bottom}>
+            <View style={styles.textContainer}>
+              <Text style={styles.bottomText}>Exercise Breakdown</Text>
+            </View>
+            <View style={styles.exerciseList}>
+              <TouchableOpacity
+                style={styles.rect}
+                activeOpacity={0.6}
+                onPress={() => navigation.navigate("Bench Press")} >
+                <View style={styles.inner} >
+                  <View style={styles.text} >
+                    <Text style={styles.mainText}>Bench Press</Text>
+                  </View>
+                  <Icon name="chevron-right" style={styles.illustration} size={25}
+                  />
                 </View>
-                <Icon name="chevron-right" style={styles.illustration} size={25}
-                />
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.rect}
-              activeOpacity={0.6}
-               >
-              <View style={styles.inner} >
-                <View style={styles.text} >
-                  <Text style={styles.mainText}>Planks</Text>
-                </View>
-                <Icon name="chevron-right" style={styles.illustration} size={25}
-                />
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.rect}
-              activeOpacity={0.6}
- >
-              <View style={styles.inner} >
-                <View style={styles.text} >
-                  <Text style={styles.mainText}>Squats</Text>
-                </View>
-                <Icon name="chevron-right" style={styles.illustration} size={25}
-                />
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.rect}
-              activeOpacity={0.6}
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.rect}
+                activeOpacity={0.6}
               >
-              <View style={styles.inner} >
-                <View style={styles.text} >
-                  <Text style={styles.mainText}>Pull Ups</Text>
+                <View style={styles.inner} >
+                  <View style={styles.text} >
+                    <Text style={styles.mainText}>Planks</Text>
+                  </View>
+                  <Icon name="chevron-right" style={styles.illustration} size={25}
+                  />
                 </View>
-                <Icon name="chevron-right" style={styles.illustration} size={25}
-                />
-              </View>
-            </TouchableOpacity>
-          </ScrollView>
-        </View>
-      </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.rect}
+                activeOpacity={0.6}
+              >
+                <View style={styles.inner} >
+                  <View style={styles.text} >
+                    <Text style={styles.mainText}>Squats</Text>
+                  </View>
+                  <Icon name="chevron-right" style={styles.illustration} size={25}
+                  />
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.rect}
+                activeOpacity={0.6}
+              >
+                <View style={styles.inner} >
+                  <View style={styles.text} >
+                    <Text style={styles.mainText}>Pull Ups</Text>
+                  </View>
+                  <Icon name="chevron-right" style={styles.illustration} size={25}
+                  />
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+      </View >
+
     );
   }
 }
+
+const data = {
+  data: [0.7, 0.4, 0.8]
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFF',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%'
   },
   top: {
-    width: '100%',
     backgroundColor: '#2C1966',
     flex: 1,
     flexDirection: 'column',
   },
   whiteBlock: {
     backgroundColor: '#fff',
-    height: 150,
-    width: 280,
+    // height: 200,
+    // width: 280,
     borderRadius: 20,
     shadowColor: "#929292",
     shadowOffset: {
@@ -110,38 +159,70 @@ const styles = StyleSheet.create({
     shadowRadius: 10,
     zIndex: 1,
     // paddingTop: 30,
-    paddingBottom: 30,
-    alignSelf: 'center'
+    // paddingBottom: 30,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  chartContainer: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  infoContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  caloriesContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 10,
+    borderRightWidth: 1,
+    borderRightColor: '#dadada',
+  },
+  timeContainer: {
+    flexDirection: 'column',
+    padding: 10
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+  infoText: {
+    textAlign: 'center',
+    fontFamily: 'Medium',
+
   },
   bottom: {
-    width: '100%',
     backgroundColor: '#F8F8F8',
     zIndex: -1,
     flex: 2,
     marginTop: -55,
     paddingTop: 55 + 90,
-    paddingHorizontal: 15,
   },
   bottomText: {
     color: 'black',
     fontFamily: 'Medium',
     letterSpacing: 1,
     textAlign: 'left'
+  },
 
+  exerciseList: {
+    paddingHorizontal: 15,
   },
 
   topText: {
     color: '#fff',
     fontFamily: 'Medium',
     letterSpacing: 1,
-    padding: 10,
     textAlign: 'left'
   },
 
-  bottomTextContainer: {
-  },
-
-  exerciseList: {
+  textContainer: {
+    width: '100%',
+    paddingHorizontal: 15,
+    paddingVertical: 15
   },
 
   rect: {

ea78569399c6ed71b61c3c663bdd2f6801994ecd
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-06-26 00:21:37 +0100

    placeholder text

--- screens/Analytics.js
+++ screens/Analytics.js
@@ -1,67 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { ProgressChart } from 'react-native-chart-kit';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function Workout() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.top}>
-        <Text style={styles.topText}>Track your Progress</Text>
-        <View style={styles.whiteBlock}>     {/* flex: 1 */}
-          <View style={styles.chartContainer}>  {/* flex: 1 */}
-            <ProgressChart data={data} width={200} height={200} strokeWidth={16} radius={32} chartConfig={chartConfig} hideLegend={true}
-          /> 
-          </View>
+import { useFonts } from '@use-expo/font';
+import { AppLoading } from 'expo';
 
-          <View style={styles.infoContainer}>  {/* flex: 1 */}
+export default function Analytics() {
+  let [fontsLoaded] = useFonts({
+    'Regular': require('../assets/fonts/Roboto-Regular.ttf'),
+    'Bold': require('../assets/fonts/Roboto-Bold.ttf'),
+    'Medium': require('../assets/fonts/Roboto-Medium.ttf'),
+  });
 
-            <View style={styles.caloriesContainer} > {/* add CSS for divider- 1px, #DADADA */}
-              <Text style={styles.infoText}>
-                Calories
-              </Text>
-              <Text style={styles.infoText}>
-                460KJ
-              </Text>
-            </View>
-            <View style={styles.timeContainer}>
-              <Text style={styles.infoText}>
-                Total Time
-              </Text>
-              <Text style={styles.infoText}>
-                40 mins
-              </Text>
-            </View>
-          </View>
-
-          <View style={styles.buttonContainer}>  {/* flex: 1, align 7 justigy: center, custom button component */}
-
-          </View>
-        </View>
+  // check if fonts are loaded
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+          <Text style={styles.topText}>User's Workout Analytics</Text>
       </View>
-      <View style={styles.bottom}>
-        <View>
-          <Text style={styles.mainText}>Exercise Breakdown</Text>
-        </View>
-        <ScrollView>
-
-        </ScrollView>
-      </View>
-    </View>
-  )
+    );
+  }
 }
-
-const data = {
-  data: [0.7]
-}
-
-// export default function Analytics() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>User's Workout Analytics</Text>
-//     </View>
-//   );
-// }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -69,24 +29,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  top: {
-    width: '100%',
-    backgroundColor: '#2C1966',
-    flex: 1
-  },
-  bottom: {
-    width: '100%',
-    backgroundColor: '#F8F8F8',
-    flex: 2,
-  },
-  mainText: {
-    color: '#FFFFF',
-    fontFamily: 'Roboto-Medium',
-  },
-
-  topText: {
-    color: 'black',
-    fontFamily: 'Roboto-Medium',
-  }
-
-});
+})

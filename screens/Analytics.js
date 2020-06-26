fac3acc7eb6a21cfffb08c149ac892340f0107e5
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-06-26 19:03:50 +0100

    commented out

--- screens/Analytics.js
+++ screens/Analytics.js
@@ -30,3 +30,78 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 })
+
+// import React, { Component } from 'react';
+// import { StyleSheet, Text, View, TouchableOpacity, AppRegistry, FlatList, ScrollView } from 'react-native';
+
+// import SetsFlatlistData from '../data/SetsFlatlistData'
+
+// import { useFonts } from '@use-expo/font';
+// import { AppLoading } from 'expo';
+
+// export default class FlatlistItem extends Component {
+//   render() {
+//     let [fontsLoaded] = useFonts({
+//       'Regular': require('../assets/fonts/Roboto-Regular.ttf'),
+//       'Bold': require('../assets/fonts/Roboto-Bold.ttf'),
+//       'Medium': require('../assets/fonts/Roboto-Medium.ttf'),
+//     });
+
+//     if (!fontsLoaded) {
+//       return <AppLoading />;
+//     } else {
+//       return (
+//         <ScrollView style={styles.container}>
+
+//           <View style={{
+//             flex: 1,
+//             flexDirection: 'column',
+//           }}>
+//             <View style={{
+//               flex: 1,
+//               flexDirection: 'row',
+//               backgroundColor: 'mediumseagreen'
+//             }}>
+              
+//               <View style={{
+//                 flex: 1,
+//                 flexDirection: 'column',
+//                 height: 100
+//               }}>
+//                 <Text style={styles.flatListItem}>{this.props.item.name}</Text>
+//                 <Text style={styles.flatListItem}>{this.props.item.foodDescription}</Text>
+//               </View>
+//             </View>
+//             <View style={{
+//               height: 1,
+//               backgroundColor: 'white'
+//             }}>
+
+//             </View>
+//           </View>
+//         </ScrollView >
+//       );
+//     }
+//   }
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#FFFF',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   flatListItem: {
+//     color: 'white',
+//     padding: 10,
+//     fontSize: 16,
+//   },
+//   },
+//   setCard: {
+//     position: 'relative',
+//     padding: 20,
+//     paddingRight: 100,
+//     borderBottomWidth: 2,
+//     borderBottomColor: '#DADADA'
+//   }
+// })

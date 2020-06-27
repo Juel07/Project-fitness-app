fb0cf56dcadb3e42ea82f6d9c123c68391f85fb8
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-06-27 14:19:37 +0100

    deleted unused code

--- App.js
+++ App.js
@@ -1,26 +1,5 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context'
 import MainTabScreen from './screens/MainTabScreen';
 
 const App = () => {

e479f1493534ff0e11526f2434093eabcf25f355
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-06-27 13:54:27 +0100

    changed rest property name

--- data/SetsFlatlistData.js
+++ data/SetsFlatlistData.js
@@ -3,25 +3,25 @@ var flatListData = [
         "key": "598a678278fee204ee51cd2c",
         "reps": '10',   
         "weight": '50',
-        "restTime": "60"
+        "rest": "60"
     },
     {
         "key": "598a684f78fee204ee51cd2f",
         "reps": '10',        
         "weight": '50',
-        "restTime": "60"
+        "rest": "60"
     },
     {
         "key": "598a687678fee204ee51cd30",
         "reps": "10",
         "weight": "50",
-        "restTime": "60"
+        "rest": "60"
     },
     {
         "key": "598a680178fee204ee51cd2e",
         "reps": "12",        
         "weight": "50",
-        "restTime": "90"
+        "rest": "90"
     }
   ];
 export default flatListData;

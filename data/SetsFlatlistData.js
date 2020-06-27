9f07a69c9450f7212a20978581c26ecfa26a8cc6
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-06-27 10:20:47 +0100

    added relevant data

--- data/SetsFlatlistData.js
+++ data/SetsFlatlistData.js
@@ -1,38 +1,27 @@
 var flatListData = [
     {
         "key": "598a678278fee204ee51cd2c",
-        "name": "Cream Tea",   
-        "foodDescription": "This is a cup of cream tea"
+        "reps": '10',   
+        "weight": '50',
+        "restTime": "60"
     },
     {
         "key": "598a684f78fee204ee51cd2f",
-        "name": "Fresh mushroom",        
-        "foodDescription": "Fresh mushroom with vegetables. This is a long line, this is a long line, this is a long line,this is a long line,this is a long line"
+        "reps": '10',        
+        "weight": '50',
+        "restTime": "60"
     },
     {
         "key": "598a687678fee204ee51cd30",
-        "name": "Japanese Oyster",
-        "foodDescription": "Oysters with ice rock"
+        "reps": "10",
+        "weight": "50",
+        "restTime": "60"
     },
     {
         "key": "598a680178fee204ee51cd2e",
-        "name": "Korean Kimchi",        
-        "foodDescription": "Traditional Korean Food"
-    },
-    {
-        "key": "598a688878fee204ee51cd31",
-        "name": "Multiple salad",     
-        "foodDescription": "Salad mixed with mushroom"
-    },
-    {
-        "key": "598a68b778fee204ee51cd32",
-        "name": "Vegetable",   
-        "foodDescription": "Fresh vegetables"
-    },
-    {
-        "key": "598a67c478fee204ee51cd2d",
-        "name": "traditional japanese salad",  
-        "foodDescription": "Very delicious Japanese Salad"
+        "reps": "12",        
+        "weight": "50",
+        "restTime": "90"
     }
-];
+  ];
 export default flatListData;
